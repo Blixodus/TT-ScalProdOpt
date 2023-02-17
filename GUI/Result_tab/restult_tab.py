ea4d326b4712b9ec3bdce8bacdf81b8ab2f6582d
Preuce <borice.lipant@gmail.com>
2023-02-17 21:37:56 +0100

    Start visualization tab elements

---
+++ GUI/Result_tab/restult_tab.py
@@ -0,0 +1,4 @@
+#a list of plot frames on the right
+#the dataset can be shown and hidden on the left
+#each plot is a lineplot (for now), with the possibility to toggle each algorithm and the time or cost
+#possibility to add, remove, copy plots
