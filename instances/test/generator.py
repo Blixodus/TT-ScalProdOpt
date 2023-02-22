d237c7641c411f7a199cde51f1affd10c8841479
Atte Torri <atte.torri@universite-paris-saclay.fr>
2023-02-22 16:23:35 +0100

    new instances

--- instances/test/generator.py
+++ instances/test/generator.py
@@ -16,14 +16,14 @@ def generate_instance(file, size, f_rank1, f_rank2, f_dims, rounded):
     # Print TT product
     file.write("v")
     for i in range(size-1):
-        file.write(" {} *{}".format(i, rank1[i+1]))
-    file.write(" {}\nv".format(size-1))
+        file.write("\t{}\t*{}".format(i, rank1[i+1]))
+    file.write("\t{}\nv".format(size-1))
     for i in range(size):
-        file.write(" *{}  ".format(dims[i]))
+        file.write("\t*{}\t".format(dims[i]))
     file.write("\nv")
     for i in range(size-1):
-        file.write(" {} *{}".format(size+i, rank2[i+1]))
-    file.write(" {}\n".format(2*size-1))
+        file.write("\t{}\t*{}".format(size+i, rank2[i+1]))
+    file.write("\t{}\n".format(2*size-1))
     file.write("d {}\n".format(size*2))
     for i in range(size-1):
         file.write("e {} {} {}\n".format(i, i+1, rank1[i+1]))
@@ -36,15 +36,18 @@ def generate_instance(file, size, f_rank1, f_rank2, f_dims, rounded):
 rng = np.random.default_rng(seed=1000)
 random_1_20 = lambda x : rng.integers(low=1, high=21)
 random_2_20 = lambda x : rng.integers(low=2, high=21)
-file = open("instance_10_random.txt", "w")
-generate_instance(file, 10, random_1_20, random_1_20, random_2_20, True)
-file.close()
-file = open("instance_20_random.txt", "w")
-generate_instance(file, 20, random_1_20, random_1_20, random_2_20, True)
-file.close()
-file = open("instance_30_random.txt", "w")
-generate_instance(file, 30, random_1_20, random_1_20, random_2_20, True)
-file.close()
-file = open("instance_40_random.txt", "w")
-generate_instance(file, 40, random_1_20, random_1_20, random_2_20, True)
-file.close()
+
+def cos_like(length):
+    centre = int(length/2)
+    def lam(x):
+        return 5+(1+np.cos(x*2*np.pi/length+np.pi))*(95/2) + rng.integers(low=-(centre-abs(x-centre))-1, high=centre-abs(x-centre)+1)
+    return lam
+    #return lambda x : 5+(1+np.cos(x*2*np.pi/length+np.pi))*(95/2) + rng.integers(low=-(centre-abs(x-centre)), high=centre-abs(x-centre))
+
+for inst in range(10, 41, 10):
+    file = open("instance_"+str(inst)+"_random.txt", "w")
+    generate_instance(file, inst, random_1_20, random_1_20, random_2_20, True)
+    file.close()
+    file = open("instance_"+str(inst)+"_cos_like.txt", "w")
+    generate_instance(file, inst, cos_like(inst), cos_like(inst), random_2_20, True)
+    file.close()
